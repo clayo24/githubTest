2c1ffcda3eb6a98b6df7eb01a53774c8baff698e
clayo24 <clay_olson@asl.org>
2023-10-11 11:34:18 +0100

    Create Age.py

---
+++ Age.py
@@ -0,0 +1,10 @@
+
+
+year = int(input("What year were you born in?: ")) #ask what year born in
+age = 2020 - year # calculates age
+print("You are " + str(age) + " years old") #Prints age
+print("I am 20 years old")#Prints another age
+print("GithubTest")
+
+
+
